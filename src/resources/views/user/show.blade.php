916ccb887fc1b7dc1597f79ae80621868e0c533b
yutotakeuchi <y.takeuchi@oplan.co.jp>
2021-04-16 17:25:42 +0900

    showページのUIを調整

--- src/resources/views/user/show.blade.php
+++ src/resources/views/user/show.blade.php
@@ -3,12 +3,12 @@
 
 <h1>{{$user[0]->name}}さんの詳細ページ</h1>
 <div class="show-wrapper">
-  <p class="show-content">名前:{{$user[0]->name}}</p>
-  <p class="show-content">年齢:{{$user[0]->age}}</p>
-  <p class="show-content">最寄り駅:{{$user[0]->station}}</p>
-  <p class="show-content">趣味:{{$user[0]->hobby}}</p>
-  <p class="show-content">得意なこと:{{$user[0]->good_at}}</p>
-  <p class="show-conetnt">自分のモチベーションのために必要なこと:{{$user[0]->motivation}}</p>
+  <p class="show-content">名前 : {{$user[0]->name}}</p>
+  <p class="show-content">年齢 : {{$user[0]->age}}</p>
+  <p class="show-content">最寄り駅 : {{$user[0]->station}}</p>
+  <p class="show-content">趣味 : {{$user[0]->hobby}}</p>
+  <p class="show-content">得意なこと : {{$user[0]->good_at}}</p>
+  <p class="show-conetnt">自分のモチベーションのために必要なこと : {{$user[0]->motivation}}</p>
 </div>
 
 <a href="/user/index">ユーザー一覧に戻る</a>
