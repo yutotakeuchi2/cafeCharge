03fbf8d8612967ee16e8526688d14abc5912993b
yutotakeuchi <y.takeuchi@oplan.co.jp>
2021-04-16 13:59:17 +0900

    フォームがnullのときのばりでーしょんを追加したかった

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -13,6 +13,10 @@ class UserController extends Controller
     }
 
     public function searchUser(Request $request){
+        if($request->name == ""){
+            $foundUsers = "存在しません";
+            return view("user/search", compact("foundUsers"));
+        }
         $userName = $request->input("search_name");
         $foundUsers = User::searchByUsers($userName);
         return view("user/search", compact("foundUsers"));
